60c2efbb275595ae8ba0e507286eb8594f28e6a1
Adalyat Nazirov <aenazirov@gmail.com>
2025-06-30 19:32:21 +0100

    add sonar step

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("application")
     id("checkstyle")
+    id("jacoco")
     id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -35,4 +37,23 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    reports {
+        html.required = false
+    }
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "AdalyatNazirov_java-project-99")
+        property("sonar.organization", "adalyatnazirov")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
